d6df46ea81605abe94b50c5215763316d95780fa
Tyler Jones <tylerjones@Tylers-MacBook-Pro.local>
2015-06-12 16:44:48 -0400

    stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,25 @@ class UsersController < ApplicationController
   end
 
   def create
+
+    all_users = User.all.map { |x| x.email }
     passhash = Digest::SHA1.hexdigest(params[:password])
-    @user = User.create(email: params[:email],
-                        password: passhash)
-    flash[:notice] = "User successfully created. Welcome!"
-    redirect_to :root
+    if all_users.include?(params[:email])
+      flash[:error] = "That e-mail already exists."
+      render :new
+
+    else @user = User.find_or_create_by(email: params[:email],
+                             password: passhash)
+      redirect_to :root
+    end
+    # redirect_to :root
+
+
+    # if User.where(email: params[:email]).count.zero?
+        # User.create(fill this in)
+        # else
+        # flash
+        # end
   end
 
   def index
