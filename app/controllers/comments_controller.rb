2fa2627dcd50e21b1b39d141c3e0aaff0345ffea
Rickard Sunden <rickard.sunden@outlook.com>
2015-06-13 16:56:16 -0400

    Added some views and redirection

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
     user_id = session[:user_id]
     content = params[:content]
     Comment.create(user_id: user_id, link_id: link_id, content: content)
-    redirect_to :root
+    redirect_to link_id_path(Link.find(link_id))
   end
 
 end
